d488fb0db50b5296ebb02068748616ea1a6d39d9
Heiduo <2878491034@qq.com>
2022-01-09 21:30:10 +0800

    al0109

--- Algorithm/LeetCode/src/com/heiduo/LeetCode2201.java
+++ Algorithm/LeetCode/src/com/heiduo/LeetCode2201.java
@@ -39,12 +39,224 @@ public class LeetCode2201 {
         /**
          * 1614. 括号的最大嵌套深度
          */
-        System.out.println("data:" + maxDepthMine("1+(2*3)/(2-1)"));
+//        System.out.println("data:" + maxDepthMine("1+(2*3)/(2-1)"));
+
+        /**
+         * 89. 格雷编码
+         */
+//        System.out.println("data:" + grayCodeMine(2).toString());
+
+        /**
+         * 1629. 按键持续时间最长的键
+         */
+//        System.out.println("data:" + slowestKeyMine(new int[]{28,65,97},"gaf"));
+
+        /**
+         * 387. 字符串中的第一个唯一字符
+         */
+//        System.out.println("data:" + firstUniqCharMine("loveleetcode"));
+
+        /**
+         * 383. 赎金信
+         */
+//        System.out.println("data:" + canConstructMine("aa","ab"));
+
+        /**
+         * 242. 有效的字母异位词
+         */
+          System.out.println("data:" + isAnagramMine("rat","nagaram"));
+
+    }
+
+    /**
+     * 242. 有效的字母异位词
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isAnagramMine(String s, String t) {
+        int[] result = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            result[s.charAt(i)-'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            result[t.charAt(i)-'a']--;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i]!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 383. 赎金信
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
+    public static boolean canConstructMine(String ransomNote, String magazine) {
+        int[] result = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            result[magazine.charAt(i)-'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            result[ransomNote.charAt(i)-'a']--;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i]<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 387. 字符串中的第一个唯一字符
+     * @param s
+     * @return
+     */
+    public static int firstUniqCharMine(String s) {
+        int[] result = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            result[s.charAt(i)-'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (result[s.charAt(i)-'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    /**
+     * 1629. 按键持续时间最长的键
+     * @param releaseTimes
+     * @param keysPressed
+     * @return
+     */
+    public static char slowestKeyMine(int[] releaseTimes, String keysPressed) {
+        char result = keysPressed.charAt(0);
+        int max = releaseTimes[0];
+        for (int i = 1; i < releaseTimes.length; i++) {
+            if (releaseTimes[i] - releaseTimes[i-1] > max){
+                result = keysPressed.charAt(i);
+                max = releaseTimes[i] - releaseTimes[i-1];
+            }else if (releaseTimes[i] - releaseTimes[i-1] == max){
+                result = (char) Math.max(result,keysPressed.charAt(i));
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 89. 格雷编码
+     *
+     * @param n
+     * @return
+     */
+    public static List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1 << n; i++) {
+            list.add((i >> 1) ^ i);
+        }
+        return list;
+    }
+
+
+    public static List<Integer> grayCodeMine(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        for (int i = 1; i < n; i++) {
+            int length = list.size();
+            int first = 1 << i;
+            System.out.println("first:" + first);
+            System.out.println("length:" + length);
+            for (int j = 0; j < length; j++) {
+                list.add(list.get(length-j-1) | first);
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * 73. 矩阵置零
+     *
+     * @param matrix
+     */
+    public static void setZeroesMine(int[][] matrix) {
+        if (matrix.length == 0) {
+            return;
+        }
+        boolean firstColumn = false;
+        boolean firstRow = false;
+        int rows = matrix.length;
+        int column = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColumn = true;
+                break;
+            }
+        }
+        for (int i = 0; i < column; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < column; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < column; i++) {
+            if (matrix[0][i] == 0) {
+                for (int j = 1; j < rows; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        if (firstColumn) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (firstRow) {
+            for (int i = 0; i < column; i++) {
+                matrix[0][i] = 0;
+            }
+        }
     }
 
 
     /**
      * 1614. 括号的最大嵌套深度
+     *
      * @param s
      * @return
      */
@@ -52,10 +264,10 @@ public class LeetCode2201 {
         int max = 0;
         int size = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i)=='('){
+            if (s.charAt(i) == '(') {
                 size++;
-                max = Math.max(max,size);
-            }else if (s.charAt(i)==')'){
+                max = Math.max(max, size);
+            } else if (s.charAt(i) == ')') {
                 size--;
             }
         }
@@ -67,10 +279,10 @@ public class LeetCode2201 {
         LinkedList<Character> stack = new LinkedList<>();
         int max = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i)=='('){
+            if (s.charAt(i) == '(') {
                 stack.push('(');
-                max = Math.max(max,stack.size());
-            }else if (s.charAt(i)==')'){
+                max = Math.max(max, stack.size());
+            } else if (s.charAt(i) == ')') {
                 stack.pop();
             }
         }
@@ -80,6 +292,7 @@ public class LeetCode2201 {
 
     /**
      * 118. 杨辉三角
+     *
      * @param numRows
      * @return
      */
@@ -89,15 +302,15 @@ public class LeetCode2201 {
         listSub.add(1);
         list.add(listSub);
         for (int i = 2; i <= numRows; i++) {
-            List<Integer> listP = list.get(i-2);
+            List<Integer> listP = list.get(i - 2);
             List<Integer> listNext = new ArrayList<>();
             for (int j = 0; j < i; j++) {
-                if (j==0){
+                if (j == 0) {
                     listNext.add(1);
-                }else if (j == i -1){
+                } else if (j == i - 1) {
                     listNext.add(1);
-                }else {
-                    listNext.add(listP.get(j-1)+listP.get(j));
+                } else {
+                    listNext.add(listP.get(j - 1) + listP.get(j));
                 }
             }
 
